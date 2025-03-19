1fabb8ace34035660347701113440339dbd102b9
Shihab Mahamud <shihab4t@gmail.com>
2025-03-19 21:09:55 +0600

    profile

--- resources/js/layouts/app2/profile.tsx
+++ resources/js/layouts/app2/profile.tsx
@@ -9,47 +9,61 @@ import {
 
 import type { TUser } from "@/types/models";
 
+import { useInitials } from "@/hooks/use-initials";
+import { useMobileNavigation } from "@/hooks/use-mobile-navigation";
+
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import { UserInfo } from "@/components/user-info";
 import { Link } from "@inertiajs/react";
 import { LogOutIcon, SettingsIcon } from "lucide-react";
 
-const Logout = () => {
-    return (
-        <DropdownMenuItem onClick={() => {}}>
-            <LogOutIcon className="mr-2 h-4 w-4" />
-            <span>Log out</span>
-        </DropdownMenuItem>
-    );
-};
-
 export const Profile = ({ user }: { user: TUser }) => {
+    const getInitials = useInitials();
+    const cleanup = useMobileNavigation();
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="h-8 w-8 rounded-full">
-                    <Avatar className="h-8 w-8">
-                        <AvatarImage src={""} alt={user?.name} />
-                        <AvatarFallback>{user?.name?.charAt(0)}</AvatarFallback>
+                <Button variant="ghost" className="size-8 rounded-full">
+                    <Avatar className="size-8">
+                        <AvatarImage src={user.avatar} alt={user?.name} />
+                        <AvatarFallback>{getInitials(user.name)}</AvatarFallback>
                     </Avatar>
                 </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-56" align="end" forceMount>
-                <DropdownMenuLabel className="font-normal">
-                    <div className="flex flex-col space-y-1">
-                        <p className="text-sm leading-none font-medium">{user?.name}</p>
-                        <p className="text-muted-foreground text-xs leading-none">{user?.email}</p>
+                <DropdownMenuLabel className="p-0 font-normal">
+                    <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                        <UserInfo user={user} showEmail={true} />
                     </div>
                 </DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem asChild>
-                    <Link href={route("settings")}>
-                        <SettingsIcon className="mr-2 h-4 w-4" />
-                        <span>Settings</span>
+                    <Link
+                        className="block w-full"
+                        href={route("profile.edit")}
+                        as="button"
+                        prefetch
+                        onClick={cleanup}
+                    >
+                        <SettingsIcon className="mr-2" />
+                        Settings
                     </Link>
                 </DropdownMenuItem>
                 <DropdownMenuSeparator />
-                <Logout />
+                <DropdownMenuItem asChild>
+                    <Link
+                        className="block w-full"
+                        method="post"
+                        href={route("logout")}
+                        as="button"
+                        onClick={cleanup}
+                    >
+                        <LogOutIcon className="mr-2" />
+                        Log out
+                    </Link>
+                </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
     );
