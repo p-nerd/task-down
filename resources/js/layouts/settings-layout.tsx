f3376bda19bf481541d6901dbbedb0ce64bb2d66
Shihab Mahamud <shihab4t@gmail.com>
2025-03-19 22:08:52 +0600

    settings-layout

--- resources/js/layouts/settings-layout.tsx
+++ resources/js/layouts/settings-layout.tsx
@@ -1,37 +1,37 @@
-import Heading from "@/components/heading";
+import type { ReactNode } from "react";
+
+import { cn } from "@/lib/utils";
+
+import { Heading } from "@/components/elements/heading";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { cn } from "@/lib/utils";
-import { type NavItem } from "@/types";
 import { Link } from "@inertiajs/react";
-import { type PropsWithChildren } from "react";
+import { LucideIcon } from "lucide-react";
 import { App2Layout } from "./app2-layout";
 
-const sidebarNavItems: NavItem[] = [
+const links: { title: string; route: string; icon: LucideIcon | null }[] = [
     {
         title: "Profile",
-        href: "/settings/profile",
+        route: "settings.profile.edit",
         icon: null,
     },
     {
         title: "Password",
-        href: "/settings/password",
+        route: "settings.password.edit",
         icon: null,
     },
     {
         title: "Appearance",
-        href: "/settings/appearance",
+        route: "settings.appearance.edit",
         icon: null,
     },
 ];
 
-export function SettingsLayout({ children }: PropsWithChildren) {
+export const SettingsLayout = ({ children }: { children: ReactNode }) => {
     // When server-side rendering, we only render the layout on the client...
     if (typeof window === "undefined") {
         return null;
     }
-
-    const currentPath = window.location.pathname;
 
     return (
         <App2Layout>
@@ -41,26 +41,24 @@ export function SettingsLayout({ children }: PropsWithChildren) {
                 <div className="flex flex-col space-y-8 lg:flex-row lg:space-y-0 lg:space-x-12">
                     <aside className="w-full max-w-xl lg:w-48">
                         <nav className="flex flex-col space-y-1 space-x-0">
-                            {sidebarNavItems.map((item) => (
+                            {links.map((item) => (
                                 <Button
-                                    key={item.href}
+                                    key={item.route}
                                     size="sm"
                                     variant="ghost"
                                     asChild
                                     className={cn("w-full justify-start", {
-                                        "bg-muted": currentPath === item.href,
+                                        "bg-muted": route().current(item.route),
                                     })}
                                 >
-                                    <Link href={item.href} prefetch>
+                                    <Link href={route(item.route)} prefetch>
                                         {item.title}
                                     </Link>
                                 </Button>
                             ))}
                         </nav>
                     </aside>
-
                     <Separator className="my-6 md:hidden" />
-
                     <div className="flex-1 md:max-w-2xl">
                         <section className="max-w-xl space-y-12">{children}</section>
                     </div>
@@ -68,4 +66,4 @@ export function SettingsLayout({ children }: PropsWithChildren) {
             </div>
         </App2Layout>
     );
-}
+};
