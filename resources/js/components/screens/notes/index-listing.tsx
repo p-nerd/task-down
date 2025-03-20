ac32f6a739e1d60bca615a1ff2be25744e60984c
Shihab Mahamud <shihab4t@gmail.com>
2025-03-21 02:27:45 +0600

    listings reordering

--- resources/js/components/screens/notes/index-listing.tsx
+++ resources/js/components/screens/notes/index-listing.tsx
@@ -1,56 +1,12 @@
 import type { TNote } from "@/types/models";
-import type { SlotItemMapArray, Swapy } from "swapy";
 
+import { useNotesReorder } from "@/hooks/use-notes-reorder";
 import { time } from "@/lib/time";
 import { cn } from "@/lib/utils";
 import { router } from "@inertiajs/react";
-import { useEffect, useMemo, useRef, useState } from "react";
-import { createSwapy, utils } from "swapy";
 
 const IndexListing = ({ notes }: { notes: TNote[] }) => {
-    const [slotItemMap, setSlotItemMap] = useState<SlotItemMapArray>(
-        utils.initSlotItemMap(notes, "id"),
-    );
-
-    const slottedItems = useMemo(
-        () => utils.toSlottedItems(notes, "id", slotItemMap),
-        [notes, slotItemMap],
-    );
-
-    const containerRef = useRef<HTMLDivElement>(null);
-    const swapyRef = useRef<Swapy | null>(null);
-
-    useEffect(
-        () => utils.dynamicSwapy(swapyRef.current, notes, "id", slotItemMap, setSlotItemMap),
-        [notes],
-    );
-
-    useEffect(() => {
-        if (containerRef.current) {
-            swapyRef.current = createSwapy(containerRef.current!, {
-                manualSwap: true,
-            });
-
-            swapyRef.current.onSwap((event) => {
-                setSlotItemMap(event.newSlotItemMap.asArray);
-            });
-
-            swapyRef.current.onSwapEnd((e) => {
-                router.patch(
-                    route("notes.reorder"),
-                    {
-                        notes: e.slotItemMap.asArray.map((t, i) => ({ id: t.item, order: i })),
-                    },
-                    {
-                        onFinish: swapyRef.current?.update,
-                    },
-                );
-            });
-        }
-        return () => {
-            swapyRef.current?.destroy();
-        };
-    }, []);
+    const { containerRef, slottedItems } = useNotesReorder(notes);
 
     return (
         <div className="grid w-full grid-cols-4 gap-3 px-10" ref={containerRef}>
