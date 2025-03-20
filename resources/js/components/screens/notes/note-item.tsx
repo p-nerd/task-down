839f645034a535c6c7ff254eaa8642bf04095727
Shihab Mahamud <shihab4t@gmail.com>
2025-03-21 03:28:58 +0600

    add content

--- resources/js/components/screens/notes/note-item.tsx
+++ resources/js/components/screens/notes/note-item.tsx
@@ -1,7 +1,10 @@
 import type { TNote } from "@/types/models";
 
+import { plugins } from "@/lib/plugins";
 import { time } from "@/lib/time";
 import { cn } from "@/lib/utils";
+
+import { MDXEditor } from "@mdxeditor/editor";
 
 export const NoteItem = ({ note, active }: { note: TNote; active?: boolean }) => {
     return (
@@ -16,6 +19,12 @@ export const NoteItem = ({ note, active }: { note: TNote; active?: boolean }) =>
             )}
         >
             <h3 className="mb-1 w-full text-base font-bold">{note.name || "Untitled Title"}</h3>
+            <MDXEditor
+                key={note.id}
+                markdown={note.content}
+                contentEditableClassName="w-full h-[100px] overflow-hidden text-sm outline-hidden prose dark:prose-invert"
+                plugins={plugins}
+            />
             <span className="text-xs font-light">{time.format.shortt(note.created_at)}</span>
         </div>
     );
