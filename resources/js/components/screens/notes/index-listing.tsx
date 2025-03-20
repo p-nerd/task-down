ec294d1cf7ab18c5a288cce977a77c168f434226
Shihab Mahamud <shihab4t@gmail.com>
2025-03-21 02:23:18 +0600

    listing order

--- resources/js/components/screens/notes/index-listing.tsx
+++ resources/js/components/screens/notes/index-listing.tsx
@@ -1,33 +1,51 @@
 import type { TNote } from "@/types/models";
-import type { Swapy } from "swapy";
+import type { SlotItemMapArray, Swapy } from "swapy";
 
 import { time } from "@/lib/time";
 import { cn } from "@/lib/utils";
 import { router } from "@inertiajs/react";
-import { useEffect, useRef } from "react";
-import { createSwapy } from "swapy";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { createSwapy, utils } from "swapy";
 
 const IndexListing = ({ notes }: { notes: TNote[] }) => {
+    const [slotItemMap, setSlotItemMap] = useState<SlotItemMapArray>(
+        utils.initSlotItemMap(notes, "id"),
+    );
+
+    const slottedItems = useMemo(
+        () => utils.toSlottedItems(notes, "id", slotItemMap),
+        [notes, slotItemMap],
+    );
+
     const containerRef = useRef<HTMLDivElement>(null);
     const swapyRef = useRef<Swapy | null>(null);
 
+    useEffect(
+        () => utils.dynamicSwapy(swapyRef.current, notes, "id", slotItemMap, setSlotItemMap),
+        [notes],
+    );
+
     useEffect(() => {
         if (containerRef.current) {
-            const swapy = createSwapy(containerRef.current, { animation: "spring" });
+            swapyRef.current = createSwapy(containerRef.current!, {
+                manualSwap: true,
+            });
 
-            swapy.onSwapEnd((e) => {
+            swapyRef.current.onSwap((event) => {
+                setSlotItemMap(event.newSlotItemMap.asArray);
+            });
+
+            swapyRef.current.onSwapEnd((e) => {
                 router.patch(
                     route("notes.reorder"),
                     {
                         notes: e.slotItemMap.asArray.map((t, i) => ({ id: t.item, order: i })),
                     },
                     {
-                        onFinish: swapy.update,
+                        onFinish: swapyRef.current?.update,
                     },
                 );
             });
-
-            swapyRef.current = swapy;
         }
         return () => {
             swapyRef.current?.destroy();
@@ -36,24 +54,27 @@ const IndexListing = ({ notes }: { notes: TNote[] }) => {
 
     return (
         <div className="grid w-full grid-cols-4 gap-3 px-10" ref={containerRef}>
-            {notes.map((n) => (
-                <div key={n.id} data-swapy-slot={n.id}>
-                    <div
-                        key={n.id}
-                        data-swapy-item={n.id}
-                        onClick={() => router.get(route("notes.show", n))}
-                        className={cn(
-                            "bg-secondary text-secondary-foreground hover:bg-primary hover:text-primary-foreground",
-                            "w-full cursor-pointer rounded-md px-2.5 py-3 transition-colors duration-75",
-                        )}
-                    >
-                        <h3 className="mb-1 w-full font-bold">{n.name}</h3>
-                        <span className="text-xs font-light">
-                            {time.format.shortt(n.created_at)}
-                        </span>
-                    </div>
-                </div>
-            ))}
+            {slottedItems.map(
+                ({ slotId, itemId, item }) =>
+                    item && (
+                        <div key={slotId} data-swapy-slot={slotId}>
+                            <div
+                                data-swapy-item={itemId}
+                                key={itemId}
+                                onClick={() => router.get(route("notes.show", item))}
+                                className={cn(
+                                    "bg-secondary text-secondary-foreground hover:bg-primary hover:text-primary-foreground",
+                                    "w-full cursor-pointer rounded-md px-2.5 py-3 transition-colors duration-75",
+                                )}
+                            >
+                                <h3 className="mb-1 w-full font-bold">{item.name}</h3>
+                                <span className="text-xs font-light">
+                                    {time.format.shortt(item.created_at)}
+                                </span>
+                            </div>
+                        </div>
+                    ),
+            )}
         </div>
     );
 };
