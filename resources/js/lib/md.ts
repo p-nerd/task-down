ce576e3297494d1fb01de067c274e852b38d447f
Shihab Mahamud <shihab4t@gmail.com>
2025-03-21 19:52:39 +0600

    md converter

--- resources/js/lib/md.ts
+++ resources/js/lib/md.ts
@@ -1,9 +1,18 @@
 import DOMPurify from "dompurify";
+
 import { micromark } from "micromark";
+import { directive, directiveHtml } from "micromark-extension-directive";
+import { frontmatter, frontmatterHtml } from "micromark-extension-frontmatter";
+import { gfm, gfmHtml } from "micromark-extension-gfm";
+import { mdx } from "micromark-extension-mdx";
+import { mdxjs } from "micromark-extension-mdxjs";
 
 const md = {
     convert: (markdown: string): string => {
-        const html = micromark(markdown);
+        const html = micromark(markdown, {
+            extensions: [directive(), frontmatter(), gfm(), mdx(), mdxjs()],
+            htmlExtensions: [directiveHtml(), frontmatterHtml(), gfmHtml()],
+        });
         const sanitizedHtml = DOMPurify.sanitize(html);
         return sanitizedHtml;
     },
