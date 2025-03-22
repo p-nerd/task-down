40916f91c002ae50bb15ebe1262520530a1f0a0e
Shihab Mahamud <shihab4t@gmail.com>
2025-03-22 23:25:41 +0600

    fix create note

--- resources/js/components/screens/notes/create-note.tsx
+++ resources/js/components/screens/notes/create-note.tsx
@@ -9,7 +9,13 @@ export const CreateNote = () => {
             size="icon"
             variant="outline"
             className="cursor-pointer"
-            onClick={() => router.post(route("notes.store"))}
+            onClick={() =>
+                router.post(route("notes.store"), undefined, {
+                    onSuccess: () => {
+                        window.location.reload();
+                    },
+                })
+            }
         >
             <NotebookPenIcon className="h-4 w-4 transition-colors" />
         </Button>
