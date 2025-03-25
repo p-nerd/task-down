95f622da4b48e5cbace458e9943dde9c38ec32a7
Shihab Mahamud <shihab4t@gmail.com>
2025-03-26 01:05:25 +0600

    fixing bug delete image will remove the id from selected image ids

--- resources/js/components/screens/images/timeline/delete-image-button.tsx
+++ resources/js/components/screens/images/timeline/delete-image-button.tsx
@@ -1,15 +1,21 @@
 import type { TImage } from "@/types/models";
 
+import { useImagesStore } from "@/states/images";
 import { router } from "@inertiajs/react";
 
 import { Confirmation } from "@/components/elements/confirmation";
 import { Trash2Icon } from "lucide-react";
 
 export const DeleteImageButton = ({ image, className }: { image: TImage; className: string }) => {
+    const { selectedImageIds, setSelectedImageIds } = useImagesStore();
+
     return (
         <>
             <Confirmation
-                onAction={() => router.delete(route("images.destroy", image.id))}
+                onAction={() => {
+                    router.delete(route("images.destroy", image.id));
+                    setSelectedImageIds(selectedImageIds.filter((id) => id !== image.id));
+                }}
                 description={`This action will permanently delete the ${image.filename} image`}
                 actionText="Delete"
             >
